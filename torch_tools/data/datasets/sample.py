7aba5f1f5952d65830ae1b876c604f77db62dbb3
gregunz <mail@gregunz.io>
2019-11-25 17:34:14 +0900

    UPT datasets -> data

---
+++ torch_tools/data/datasets/sample.py
@@ -0,0 +1,60 @@
+import random
+
+from torch.utils.data import Dataset
+
+
+class Sample(Dataset):
+    def __init__(self, dataset: Dataset, num_samples: int = None, random_sampling=False):
+        assert num_samples is None or num_samples > 0, 'need at least one sample in the dataset'
+
+        self.dataset = dataset
+        n = len(dataset)
+
+        if num_samples is None:
+            num_samples = n
+
+        self.all_indices = list(range(n))
+
+        if random_sampling:
+            random.shuffle(self.all_indices)
+
+        self.indices = [i % n for i in self.all_indices[:num_samples]]
+
+    def __getitem__(self, idx):
+        return self.dataset[self.indices[idx]]
+
+    def __len__(self):
+        return len(self.indices)
+
+# class Sample(Dataset):
+#     def __init__(self, dataset: Dataset, num_samples: int = None, random_sampling=False):
+#         assert num_samples is None or num_samples > 0, 'need at least one sample in the dataset'
+#
+#         self.random_sampling = random_sampling
+#         self.dataset = dataset
+#
+#         n = len(dataset)
+#
+#         if num_samples is None:
+#             num_samples = n
+#
+#         self.all_indices = list(range(n))
+#
+#         if random_sampling:
+#             random.shuffle(self.all_indices)
+#             self.n_idx = 0
+#
+#         self.indices = [i % n for i in self.all_indices[:num_samples]]
+#
+#     def __getitem__(self, idx):
+#         if self.random_sampling:
+#             self.n_idx += 1
+#             if self.n_idx % len(self) == 0:
+#                 self.n_idx = 0
+#                 random.shuffle(self.all_indices)
+#                 self.indices = [i % len(self.dataset) for i in self.all_indices[:num_samples]]
+#
+#         return self.dataset[self.indices[idx]]
+#
+#     def __len__(self):
+#         return len(self.indices)
---
+++ torch_tools/data/samplers/random_sampler.py
@@ -0,0 +1,51 @@
+import torch
+from torch.utils.data import Sampler
+
+
+class RandomSampler(Sampler):
+    r"""Samples elements randomly. If without replacement, then sample from a shuffled dataset.
+    User can specify :attr:`num_samples` to draw.
+
+    Arguments:
+        data_source (Dataset): dataset to sample from
+        replacement (bool): samples are drawn with replacement if ``True``, default=``False``
+        num_samples (int): number of samples to draw, default=`len(dataset)`.
+    """
+
+    def __init__(self, data_source, replacement=False, num_samples=None):
+        super().__init__(data_source)
+        self.data_source = data_source
+        self.replacement = replacement
+        self._num_samples = num_samples
+        n = len(self.data_source)
+        self.all_indices = torch.randperm(n).tolist()
+
+        if not isinstance(self.replacement, bool):
+            raise ValueError("replacement should be a boolean value, but got "
+                             "replacement={}".format(self.replacement))
+
+        if not isinstance(self.num_samples, int) or self.num_samples <= 0:
+            raise ValueError("num_samples should be a positive integer "
+                             "value, but got num_samples={}".format(self.num_samples))
+
+    @property
+    def num_samples(self):
+        # dataset size might change at runtime
+        if self._num_samples is None:
+            return len(self.data_source)
+        return self._num_samples
+
+    def __iter__(self):
+        n = len(self.data_source)
+        if self.replacement:
+            return iter(torch.randint(high=n, size=(self.num_samples,), dtype=torch.int64).tolist())
+
+        self.all_indices = self.all_indices[self.num_samples:]
+
+        if len(self.all_indices) == 0:
+            self.all_indices = torch.randperm(n).tolist()
+
+        return iter(self.all_indices[:self.num_samples])
+
+    def __len__(self):
+        return self.num_samples
