30e877984b9aab6f9b2ee718ef4b1ba583f187aa
gregunz <mail@gregunz.io>
2019-12-09 16:51:08 +0900

    ADD GradCam and add GradCam + Guided Backprop to init

--- torch_tools/models/vision/util/__init__.py
+++ torch_tools/models/vision/util/__init__.py
@@ -1,2 +1,4 @@
 from .deep_convolutional_coders import DCEncoder, DCDecoder
+from .grad_cam import GradCAM
+from .guided_backprop import GuidedBackProp
 from .pyramid_blocks import PyramidDown, PyramidUp
